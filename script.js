650d9e6ac4952240af5663cbbde879358caa0e4e
Franklin Lorran <lorranfranklin.dev@gmail.com>
2024-05-17 08:40:00 -0300

    add jquery

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 $(document).ready(function () {
+  const taskInput = $(".input-field input");
+  const filters = $(".filters span");
+
+
+  filters.on("click", function () {
+    $(".active")
+  })
+  
 });
